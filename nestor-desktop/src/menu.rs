af4298d7ecb93b7dd936e4faf537b3ddfa366344
Daniel Ferreira <danferreiradev@gmail.com>
2024-07-18 03:18:45 -0300

    update menu

--- nestor-desktop/src/menu.rs
+++ nestor-desktop/src/menu.rs
@@ -22,34 +22,15 @@ impl<'a> Menu<'a> {
         }
     }
 
-    pub fn item(mut self, label: &str, message: Message) -> Self {
-        let item: button::Button<Message, iced::Theme, iced::Renderer> =
-            button(text(label.to_string()))
-                .style(|_theme, status| match status {
-                    button::Status::Hovered => button::Style {
-                        text_color: Color::WHITE,
-                        background: Some(Color::from_rgba(0.9, 0.0, 0.0, 0.5).into()),
-                        ..Default::default()
-                    },
-                    _ => button::Style {
-                        text_color: Color::WHITE,
-                        ..Default::default()
-                    },
-                })
-                .on_press(message)
-                .width(Length::Fill);
-
-        let menu_item = menu::Item::new(item);
-        self.items.push(menu_item);
-        self
-    }
-
-    pub fn build(self) -> Item<'a, Message, Theme, Renderer> {
-        let item = button(text(self.label))
+    fn menu_button_item(
+        label: String,
+        message: Message,
+    ) -> button::Button<'a, Message, iced::Theme, iced::Renderer> {
+        button(text(label))
             .style(|_theme, status| match status {
                 button::Status::Hovered => button::Style {
                     text_color: Color::WHITE,
-                    background: Some(Color::from_rgba(0.9, 0.0, 0.0, 0.5).into()),
+                    background: Some(Color::from_rgb8(159, 28, 36).into()),
                     ..Default::default()
                 },
                 _ => button::Style {
@@ -57,7 +38,18 @@ impl<'a> Menu<'a> {
                     ..Default::default()
                 },
             })
-            .on_press(Message::Dummy);
+            .on_press(message)
+    }
+
+    pub fn item(mut self, label: &'a str, message: Message) -> Self {
+        let item = Menu::menu_button_item(label.to_string(), message).width(Length::Fill);
+
+        self.items.push(menu::Item::new(item));
+        self
+    }
+
+    pub fn build(self) -> Item<'a, Message, Theme, Renderer> {
+        let item = Menu::menu_button_item(self.label, Message::Dummy);
 
         menu::Item::with_menu(
             item,
@@ -71,9 +63,9 @@ pub(crate) fn menu_bar(
 ) -> MenuBar<Message, Theme, Renderer> {
     menu::MenuBar::new(items)
         .style(|_theme, _status| menu_bar::Style {
-            bar_background: Color::BLACK.into(),
+            bar_background: Color::from_rgb8(94, 94, 94).into(),
             bar_background_expand: 0.into(),
-            menu_background: Color::BLACK.into(),
+            menu_background: Color::from_rgb8(94, 94, 94).into(),
             menu_border: Border {
                 radius: 0.0.into(),
                 ..Default::default()
